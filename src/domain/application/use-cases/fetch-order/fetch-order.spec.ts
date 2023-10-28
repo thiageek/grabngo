f5f46c9b5f4aa73fa37407f7b807ade34a59f686
Felipe Pereira <felipepereiracg@gmail.com>
2023-10-28 17:33:11 -0300

    adjustment import

--- src/domain/application/use-cases/fetch-order/fetch-order.spec.ts
+++ src/domain/application/use-cases/fetch-order/fetch-order.spec.ts
@@ -1,16 +1,16 @@
-import { FetchOrders } from '.'
+import { FetchOrder } from '.'
 import { OrderRepository } from '../../repositories/order-repository'
 import { MockOrderRepository } from '@test/repositories/mock-order-repository'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Order } from '@/domain/enterprise/entities/order'
 import { OrderItem } from '@/domain/enterprise/entities/order-item'
 
-let sut: FetchOrders
+let sut: FetchOrder
 let repository: OrderRepository
 describe('Fetch orders', () => {
   beforeEach(() => {
     repository = new MockOrderRepository()
-    sut = new FetchOrders(repository)
+    sut = new FetchOrder(repository)
   })
 
   it('Should be able list orders', async () => {
