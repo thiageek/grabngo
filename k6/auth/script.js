5a100cc9d0d406b09efc38a102ecfe0ff740d6ce
Felipe Pereira <felipepereiracg@gmail.com>
2024-01-29 23:26:41 -0300

    test: adjustments test

--- k6/auth/script.js
+++ k6/auth/script.js
@@ -1,18 +1,18 @@
 import http from 'k6/http'
-import { sleep } from 'k6'
+import { check, sleep } from 'k6'
 
 export const options = {
   stages: [
-    { duration: '10m', target: 20000 },
-    { duration: '30m', target: 2000 },
-    { duration: '5m', target: 1000 },
+    { duration: '1m', target: 1000 },
+    { duration: '3m', target: 3000 },
+    { duration: '1m', target: 1000 },
   ],
 }
 export default function () {
   const data = { login: 'user', password: 'password' }
   let res = http.post('http://server/api/v1/auth/sig-in', data)
 
-  check(res, { 'success login': (r) => r.status === 200 })
+  check(res, { 'success login': (r) => r.status === 201 })
 
-  sleep(10)
+  sleep(5)
 }
